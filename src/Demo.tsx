f03257a5a662ede8df2936c2e7c1dcb9c347e74c
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2021-01-17 22:21:41 +0100

    finito

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,13 +1,83 @@
-import React from 'react';
-import { Slider } from './Slider';
+import React, { useCallback, useState } from 'react';
+import { Slider, sliderConfig } from './Slider';
 
 import styles from './Demo.module.css';
+import { motion, Transition, useAnimation, Variants } from 'framer-motion';
+
+const baseHandleTransition: Transition = {
+	restDelta: 0,
+};
+
+const curtainVariants: Record<string, Variants> = {
+	left: {
+		hidden: {
+			x: '-100vw',
+		},
+		visible: {
+			x: '-50vw',
+		},
+	},
+	right: {
+		hidden: {
+			x: '100vw',
+		},
+		visible: {
+			x: '50vw',
+		},
+	},
+};
 
 export const Demo = () => {
+	const curtainControls = useAnimation();
+
+	const handleDrag = useCallback((value, max) => {}, []);
+
+	const handleDragEnd = useCallback(
+		({ controls, dropRatio, info, maxPosition }: Slider.DragEndPayload) => {
+			if (dropRatio > sliderConfig.snapHandleToEndRatio) {
+				controls
+					.start({
+						x: maxPosition,
+						transition: baseHandleTransition,
+					})
+					.then(() => {
+						curtainControls.start('visible', {
+							delay: 0.5,
+							restDelta: 0,
+						});
+					});
+			} else {
+				controls.start({
+					x: 0,
+					transition: baseHandleTransition,
+				});
+			}
+		},
+		[],
+	);
+
+	const handleDragStart = useCallback(() => {}, []);
+
 	return (
 		<div className={styles.root}>
+			<motion.div
+				initial="hidden"
+				variants={curtainVariants.left}
+				animate={curtainControls}
+				className={styles.leftCurtain}
+			/>
+			<motion.div
+				initial="hidden"
+				variants={curtainVariants.right}
+				animate={curtainControls}
+				className={styles.rightCurtain}
+			/>
 			<div className={styles.container}>
-				<Slider />
+				<Slider
+					onSlideChange={handleDrag}
+					onDragEnd={handleDragEnd}
+					onDragStart={handleDragStart}
+				/>
 			</div>
 		</div>
 	);
--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,14 +1,45 @@
-import { motion, PanInfo, useAnimation, useMotionValue, useTransform } from 'framer-motion';
-import { useCallback, useEffect, useRef } from 'react';
-import { ReactComponent as ShutDownIcon } from './shut-down.svg';
+import {
+	AnimatePresence,
+	AnimationControls,
+	motion,
+	PanInfo,
+	useAnimation,
+	useMotionValue,
+	useTransform,
+} from 'framer-motion';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import styles from './Slider.module.css';
 
-const config = {
+export const sliderConfig = {
 	railHeight: 62,
 	thumbSize: 56,
 	defaultRailWidth: 274,
+	snapHandleToEndRatio: 0.6,
+	labelOpacity: {
+		outputRange: [1, 0, 0, 1],
+	},
+	labelPosition: {
+		inputStart: 100,
+		outputRange: [0, -70],
+	},
 };
+
+export declare namespace Slider {
+	export type DragEndPayload = {
+		info: PanInfo;
+		maxPosition: number;
+		dropRatio: number;
+		controls: AnimationControls;
+	};
+
+	export type Props = {
+		onSlideChange?(value: number, max: number): void;
+		onDragStart?(): void;
+		onDragEnd?(opts: DragEndPayload): void;
+		className?: string;
+	};
+}
 
 export const Slider = ({
 	className,
@@ -16,67 +47,71 @@ export const Slider = ({
 	onDragEnd,
 	onDragStart,
 	...props
-}: {
-	onSlideChange?(value: number, max: number): void;
-	onDragStart?(): void;
-	onDragEnd?(info: PanInfo, finish: boolean): void;
-	className?: string;
-}) => {
+}: Slider.Props) => {
 	const railRef = useRef<null | HTMLDivElement>(null);
+	const [switched, setSwitched] = useState(false);
 
 	const controls = useAnimation();
 	const xPosition = useMotionValue(0);
 
 	const getRailWidth = () =>
-		railRef.current?.getBoundingClientRect().width || config.defaultRailWidth;
+		railRef.current?.getBoundingClientRect().width || sliderConfig.defaultRailWidth;
 
+	/**
+	 * Update parent component about position change
+	 */
 	useEffect(() => {
-		if (!railRef.current || !onSlideChange) {
+		if (!railRef.current) {
 			return;
 		}
 
 		xPosition.onChange((value) => {
-			onSlideChange(value, getRailWidth() - config.thumbSize);
+			if (value > getRailWidth() * sliderConfig.snapHandleToEndRatio) {
+				setSwitched(true);
+			} else {
+				setSwitched(false);
+			}
+
+			onSlideChange && onSlideChange(value, getRailWidth() - sliderConfig.thumbSize);
 		});
 
 		return () => {
 			xPosition.clearListeners();
 		};
-	}, [xPosition, onSlideChange, railRef.current]);
+	}, [xPosition, onSlideChange, railRef]);
 
-	const labelOpacity = useTransform(xPosition, [0, 30, 130, 200], [1, 0, 0, 1]);
+	const labelOpacity = useTransform(
+		xPosition,
+		[
+			0,
+			30,
+			getRailWidth() * sliderConfig.snapHandleToEndRatio,
+			getRailWidth() * sliderConfig.snapHandleToEndRatio + 10,
+		],
+		sliderConfig.labelOpacity.outputRange,
+	);
 
-	const labelPosition = useTransform(xPosition, [100, getRailWidth()], [0, -70]);
+	const labelPosition = useTransform(
+		xPosition,
+		[sliderConfig.labelPosition.inputStart, getRailWidth()],
+		sliderConfig.labelPosition.outputRange,
+	);
 
 	const handleDragEnd = useCallback(
 		(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
 			const dropPosition = xPosition.get();
+			const railWidth = getRailWidth();
+			const dropRatio = xPosition.get() / railWidth;
 
-			if (dropPosition > getRailWidth() * 0.7) {
-				controls
-					.start({
-						x: getRailWidth() - config.thumbSize,
-						transition: {
-							restDelta: 0,
-						},
-					})
-					.then(() => {
-						onDragEnd && onDragEnd(info, true);
-					});
-			} else {
-				controls
-					.start({
-						x: 0,
-						transition: {
-							restDelta: 0,
-						},
-					})
-					.then(() => {
-						onDragEnd && onDragEnd(info, false);
-					});
-			}
+			onDragEnd &&
+				onDragEnd({
+					dropRatio: dropRatio,
+					controls: controls,
+					info: info,
+					maxPosition: railWidth - sliderConfig.thumbSize,
+				});
 		},
-		[controls, onDragEnd],
+		[controls, onDragEnd, xPosition],
 	);
 
 	return (
@@ -94,10 +129,30 @@ export const Slider = ({
 						animate={controls}
 						style={{ x: xPosition }}
 					>
-						<ShutDownIcon className={styles.icon} />
+						<motion.svg
+							className={styles.icon}
+							viewBox="0 0 18 18"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M5 2C2.60879 3.38324 1 5.96861 1 8.92974C1 13.348 4.58172 16.9297 9 16.9297C13.4183 16.9297 17 13.348 17 8.92974C17 5.96861 15.3912 3.38324 13 2"
+								stroke="currentColor"
+								strokeWidth="1.5"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+							<path
+								d="M9 9.00005V0.800049"
+								stroke="currentColor"
+								strokeWidth="1.5"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</motion.svg>
 					</motion.div>
 					<motion.span style={{ opacity: labelOpacity, x: labelPosition }} className={styles.label}>
-						Slide to shut down
+						{switched ? <span>Release to shut down</span> : <span>Slide to shut down</span>}
 					</motion.span>
 				</div>
 			</div>
